388c91cddb73fa6936274402c57d3c7d9293797d
JIAWea <1552937000@qq.com>
2020-05-31 13:38:50 +0800

    flag

---
+++ flag/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+
+	// 第一个为flag名称
+	// 第二个为默认值
+	// 第三个为提示语
+	fileName := flag.String("filename", "logfile", "File name for the log file")
+	logLevel := flag.Int("loglevel", 0, "An integer value for Level (0-4)")
+	isEnable := flag.Bool("enable", false, "A boolean value for enabling log options")
+	
+	var num int
+	// 判定变量
+	flag.IntVar(&num, "num", 25, "An integer value")
+
+	// Parse parses flag definitions from the argument list.
+	flag.Parse()
+
+	// Get the values from pointers
+	fmt.Println("filename:", *fileName)
+	fmt.Println("loglevel:", *logLevel)
+	fmt.Println("enable:", *isEnable)
+
+	// Get the value from a variable
+	fmt.Println("num:", num)
+
+	// 返回没有定义的flag
+	args := flag.Args()
+	// fmt.Printf("args type: %T\n", args)  // []string
+	if len(args) > 0 {
+		fmt.Println("没有申明的flag名称:")
+		// Print the arguments
+		for _, v := range args {
+			fmt.Println(v)
+		}
+	}
+
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module examples
 
 go 1.12
 
-require github.com/go-playground/locales v0.13.0 // indirect
+require (
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/nats-io/nats.go v1.9.2 // indirect
+)
